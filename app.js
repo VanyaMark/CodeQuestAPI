7381d212a88b14400b654b4e2a195e71827a1692
SharmarAndrey <sharnarandrey@gmail.com>
2024-11-11 19:19:47 +0100

    Install helmet fo security

--- app.js
+++ app.js
@@ -1,33 +1,37 @@
 const express = require("express");
 const { connectDB } = require("./utils/db");
 const dotenv = require("dotenv");
+const helmet = require("helmet"); // add helmet
 const indexRouter = require("./routes/index.routes");
 const apiRouter = require("./routes/api.routes");
-const { getRandomQuestion, getRandomQuestionWithoutCodeExamples } = require('./services/question.services');
-const { shuffleArray } = require('./utils/utils')
+const {
+  getRandomQuestion,
+  getRandomQuestionWithoutCodeExamples,
+} = require("./services/question.services");
+const { shuffleArray } = require("./utils/utils");
 
 dotenv.config();
 
 const app = express();
+app.use(helmet()); //use helmet to all routes
 app.use(express.urlencoded({ extended: true }));
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 app.use("/", indexRouter);
 app.use("/api", apiRouter);
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.get('/daily-question', async (req, res) => {
-
+app.get("/daily-question", async (req, res) => {
   // Obtener la pregunta correspondiente al día
   const questions = await getRandomQuestionWithoutCodeExamples();
-  const questionsWithShuffledAnswers = questions.map(question => {
+  const questionsWithShuffledAnswers = questions.map((question) => {
     return {
-        ...question,
-        answerOptions: shuffleArray(question.answerOptions)
+      ...question,
+      answerOptions: shuffleArray(question.answerOptions),
     };
-});
+  });
   // Renderizar la página con la pregunta y las opciones
-  res.render('home',  {questionsWithShuffledAnswers} );
-})
+  res.render("home", { questionsWithShuffledAnswers });
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, async () => {
