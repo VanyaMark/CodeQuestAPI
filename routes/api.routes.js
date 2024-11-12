6f995ccfc50c430530a37f2da5bdd1bdb3a986fa
kevmhughes <kevhughes24@hotmail.com>
2024-11-12 22:18:50 +0100

    Customise request limits on one endpoint: /api/v1/questions/ai

--- routes/api.routes.js
+++ routes/api.routes.js
@@ -1,25 +1,24 @@
 const express = require("express");
-const limit = require("express-limit").limit;
+const { rateLimit } = require ('express-rate-limit')
 const router = express.Router();
 const apiControllers = require("../controllers/api.controllers");
+
+// this limits the amount of requests to the "/api/v1/questions/ai" endpoint to 15 requests per minute
+
+const customLimiter = rateLimit({
+  windowMs: 60 * 1000, // 60 seconds
+  limit: 15,
+  handler: (req, res) => {
+    res.status(429).json({
+      error: "You have exceeded the request limit for this endpoint.",
+    });
+  },
+});
 
 router.get("/v1/questions/random", apiControllers.getRandomQuestions);
 
 router.get(
-  "/v1/questions/ai",
-  limit({
-    max: 10, // 10 requests
-    period: 60 * 1000, // per minute (60 seconds)
-    onLimitReached: (req, res) => {
-      // Custom response for when the limit is reached
-      res.status(429).json({
-        error: "Too Many Requests",
-        message:
-          "You have reached the maximum number of requests. Please try again later.",
-      });
-    },
-  }),
-  apiControllers.getAiQuestions
+  "/v1/questions/ai", customLimiter, apiControllers.getAiQuestions
 );
 
 module.exports = router;
